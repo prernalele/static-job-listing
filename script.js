977cdaf7b2a968803fde33a9d7d6fe05ccfba2c8
Prerna Lele <prernalele@gmail.com>
2024-02-22 17:00:42 -0500

    (feat): adds method to filter job rows

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-bodyTag = document.getElementById("")
+const roleClassAll = document.querySelectorAll(".role");
+const levelClassAll = document.querySelectorAll(".level");
+//console.log(levelClassAll);
+roleClassAll.forEach((role) => console.log(role.dataset.role));
+levelClassAll.forEach((level) => console.log(level.dataset.level));
+const roleClassAllArray = Array.from(roleClassAll);
+console.log(roleClassAllArray);
 
-/* fetch('./data.json')
-    .then((response)=> response.json())
-    .then((json)=> {
-        console.log(json)
-        for ( let i=0; i< json.length; i++ ) {
-           parentDiv = document.createElement("div")
-           parentDiv.id = 'parent' + json[i].id
-          document.body.appendChild(parentDiv)
-        }
-        const parent1 = document.getElementById('parent1')
-        
-    }) */
-
-
-/*  const listingOne = document.getElementById("1")
-
-console.log(listingOne.dataset)
-
-const jobListArray = Object.entries(listingOne.dataset)
-console.log("jobListArray", jobListArray)
-
-console.log('jobListArray keys', Object.keys(listingOne.dataset))
-
-listingOne.innerHTML = Object.keys(listingOne.dataset)[0]
-listingOne.inner */ 
-const listOfRoles = document.getElementsByClassName("role")
-const jobRows = document.getElementsByClassName("jobRows")
-console.log(listOfRoles[0].dataset["role"])
-
-for (i = 0; i < jobRows.length; i++) {
-  arrayOfRoles = [];
-  console.log(listOfRoles[i].dataset["role"]);
-  const roleName = listOfRoles[i].dataset["role"];
-  arrayOfRoles.push(roleName);
+//method to filter jobs by filter tags
+function filterFunction() {
+  const filteredOutRows = roleClassAllArray.filter((role) => {
+    return !(role.dataset.role === "frontend");
+  });
+  console.log(filteredOutRows);
+  filteredOutRows.map((row) => {
+    return (row.parentNode.parentNode.classList.value = "hidden");
+  });
 }

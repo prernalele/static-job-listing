b4f4fd4d723cf84556fd865e711253cd0257167e
Prerna Lele <prernalele@gmail.com>
2024-02-24 15:35:03 -0500

    (feat): makes the method more generic allowing reuse

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ const roleClassAllArray = Array.from(roleClassAll);
 console.log(roleClassAllArray);
 
 //method to filter jobs by filter tags
-function filterFunction() {
-  const filteredOutRows = roleClassAllArray.filter((role) => {
-    return !(role.dataset.role === "frontend");
-  });
-  console.log(filteredOutRows);
+function filterFunction(filterKey, element) {
+  // the text value that got clicked and needs to be filter by
+  const filterValue = element.textContent.toLowerCase();
+  // grab every span that's in filter section
+  filterKeyClassAll = document.querySelectorAll("." + filterKey);
+  // make an array
+  const filterKeyClassAllArray = Array.from(filterKeyClassAll);
+  // run filter function on this array to form an array of the ones that don't match with filter value
+  const filteredOutRows = filterKeyClassAllArray.filter(
+    (row) => !(row.dataset[filterKey] === filterValue)
+  );
+  // map over these to add hidden class on each span that needs to disappear from the results
   filteredOutRows.map((row) => {
     return (row.parentNode.parentNode.classList.value = "hidden");
   });
